92d7980b4aa960f6d53f83361123b496f7714602
amin283 <149660938+amin283@users.noreply.github.com>
2023-11-27 15:18:39 +0330

    Update soal1.cpp

--- HW4/soal1.cpp
+++ HW4/soal1.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using namespace std;
 
+main(){
+	int a,b,total,  multi, subtract; //multiplication:multi
+	int remainder;
+	cout << "enter two numbers:";
+	cin >> a >> b;
+	
+	total=a+b;
+	subtract=a-b;
+	multi=a*b;
+	remainder=a%b;
+
+  cout << "the total of the two numbers is: " << total << endl;
+  cout << "the subtraction of the two numbers is: " << subtract << endl;
+  cout << "the multiplication of the two numbers is: " << multi << endl;
+  cout << "the remainder of the division of the two numbers is: " << remainder << endl;
+}
