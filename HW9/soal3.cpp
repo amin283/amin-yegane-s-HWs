534577dedd38216bdbc6a8fb97adcb6597567c70
amin283 <149660938+amin283@users.noreply.github.com>
2024-01-12 21:02:53 +0330

    Create soal3.cpp

---
+++ HW9/soal3.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n = 10;
+    int fib[n];
+    fib[0] = 0;
+    fib[1] = 1;
+    for (int i = 2; i < n; i++) {
+        fib[i] = fib[i-1] + fib[i-2];
+    }
+
+    cout << "first 10 fib: ";
+    for (int i = 0; i < n; i++) {
+        cout << fib[i] << " ";
+    }
+
+    return 0;
+}
